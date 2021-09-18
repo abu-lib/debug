744c874ca573eff7099e0e4f621a2491dd59cc07
Francois Chabot <francois.chabot.dev@gmail.com>
2021-09-18 15:03:33 -0400

    Installation

--- cmake/abu_lib.cmake
+++ cmake/abu_lib.cmake
@@ -1,21 +1,34 @@
 message(STATUS "Configuring ${PROJECT_NAME} @ ${PROJECT_VERSION}")
 
-set(${PROJECT_NAME}_VERSION ${PROJECT_VERSION} CACHE INTERNAL "")
+set(${PROJECT_NAME}_version ${PROJECT_VERSION} CACHE INTERNAL "")
 
-set(ABU_BUILD_ALL_TESTS OFF CACHE BOOL "Build tests for all abu libs")
-SET(ABU_COVERAGE OFF CACHE BOOL "Creates coverage report")
-SET(ABU_BENCHMARKS OFF CACHE BOOL "Benchmark abu libraries")
 SET(ABU_REPO_PREFIX https://github.com/abu-lib/ CACHE STRING "")
 
-get_directory_property(hasParent PARENT_DIRECTORY)
-if(hasParent)
-  set(${PROJECT_NAME}_BUILD_TESTS ${ABU_BUILD_ALL_TESTS} CACHE BOOL "")
-else()
-  set(${PROJECT_NAME}_BUILD_TESTS ON CACHE BOOL "")
+if(NOT DEFINED ${PROJECT_NAME}_master_project)
+    if(CMAKE_CURRENT_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR)
+        message(STATUS "${PROJECT_NAME} built as master project")
+        set(${PROJECT_NAME}_master_project ON)
+    else()
+        set(${PROJECT_NAME}_master_project OFF)
+    endif()
 endif()
 
+get_directory_property(hasParent PARENT_DIRECTORY)
+
+set(${PROJECT_NAME}_build_tests ${PROJECT_NAME}_master_project CACHE BOOL "")
+set(${PROJECT_NAME}_install ${PROJECT_NAME}_master_project CACHE BOOL "")
+set(${PROJECT_NAME}_install_dependencies ON CACHE BOOL "(Only if ${PROJECT_NAME}_install): Also install other required abu libraries ")
+set(${PROJECT_NAME}_coverage OFF CACHE BOOL "")
+set(${PROJECT_NAME}_build_benchmarks OFF CACHE BOOL "")
+
 include(FetchContent)
+
 function(abu_fetch LIB VER)
+  if(${PROJECT_NAME}_install AND ${PROJECT_NAME}_install_dependencies)
+    set(abu_${LIB}_install ON CACHE BOOL "")
+    set(abu_${LIB}_install_dependencies ON CACHE BOOL "")
+  endif()
+
   if(NOT TARGET abu_${LIB})
     FetchContent_Declare(abu_${LIB} 
       GIT_REPOSITORY ${ABU_REPO_PREFIX}${LIB}.git 
@@ -23,8 +36,8 @@ function(abu_fetch LIB VER)
     )
     FetchContent_MakeAvailable(abu_${LIB})
   endif()
-  if(${abu_${LIB}_VERSION} VERSION_LESS ${VER})
-    message(SEND_ERROR "abu_${LIB} ${abu_${LIB}_VERSION} is too old, ${PROJECT_NAME} needs > ${VER}")
+  if(${abu_${LIB}_version} VERSION_LESS ${VER})
+    message(SEND_ERROR "abu_${LIB} ${abu_${LIB}_version} is too old, ${PROJECT_NAME} needs > ${VER}")
   endif()
 endfunction()
 
@@ -32,6 +45,7 @@ function(abu_configure_test_target TGT)
   if (NOT TARGET GTest::gtest_main)
     find_package(GTest 1.11 QUIET)
   endif()
+
   if (NOT TARGET GTest::gtest_main)
       message(STATUS "fetching GTest from github repo")
       FetchContent_Declare(
@@ -40,10 +54,12 @@ function(abu_configure_test_target TGT)
         GIT_TAG        release-1.11.0
       )
 
+      option(INSTALL_GTEST "Install gtest" OFF)
+      set(INSTALL_GTEST OFF)
       FetchContent_MakeAvailable(googletest)
   endif()
 
-  if(ABU_COVERAGE)
+  if(${PROJECT_NAME}_coverage)
       target_compile_options(${TGT} PRIVATE -fprofile-arcs -ftest-coverage)
       target_link_libraries(${TGT} PRIVATE gcov)
   endif()
